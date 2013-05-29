978b0cb5081d708251a315780a562b1ef2e3f7af
Victor Hugo Arias Valencia <hugovic1182@gmail.com>
2013-05-29 17:38:59 -0500

    Arreglo de error en una url que cometí

--- protected/config/main.php
+++ protected/config/main.php
@@ -41,7 +41,6 @@ return array(
 			'urlFormat'=>'path',
 			'showScriptName' => false,			
 			'rules'=>array(				
-				'convocatoria' => 'site/convocatoria', 
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
