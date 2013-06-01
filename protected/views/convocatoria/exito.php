bb386af0ae0e44c605e0dde9a9b6a386035fd083
Victor Arias <hugovic1182@gmail.com>
2013-06-01 14:42:13 -0500

    Modificada página de éxito

--- protected/views/convocatoria/exito.php
+++ protected/views/convocatoria/exito.php
@@ -1,6 +1,6 @@
 <div class="row-fluid">
 	<div class="span12">
-		<h2>Inscripción Exitosa</h2>
-		
+		<h2>¡Recibimos tu propuesta correctamente!</h2>
+		<p>Recuerda tener en cuenta las fechas de la convocatoria</p>
 	</div>		
 </div>
