a90c8ca741e9cf756326f111948a52f4b26a2509
Victor Arias <hugovic1182@gmail.com>
2013-06-30 20:46:41 -0500

    Más pruebas de redimension.........

--- protected/controllers/DirectorioController.php
+++ protected/controllers/DirectorioController.php
@@ -204,8 +204,9 @@ class DirectorioController extends Controller
 	public function actionGenerarThumbs()
 	{
 		$c = new CDbCriteria;
-		$c->limit = 50;
-		$perfiles = Perfiles::model()->findAll();
+		$c->limit = 40;
+		$c->offset = 40;
+		$perfiles = Perfiles::model()->findAll($c);
 	    foreach($perfiles as $perfil)
 	    {
 	      //CVarDumper::dump($perfil->attributes);
