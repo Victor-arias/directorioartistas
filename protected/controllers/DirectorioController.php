e9a43634e0470d961518e05d1370a6714b25cd7a
Victor Arias <hugovic1182@gmail.com>
2013-06-30 20:00:29 -0500

    Más pruebas de redimension...

--- protected/controllers/DirectorioController.php
+++ protected/controllers/DirectorioController.php
@@ -214,12 +214,12 @@ class DirectorioController extends Controller
 	      	{
 	      		//echo $foto->src;
 	      		$pedazos = explode('/', $foto->src);
-	      		$nn = 't_' . $pedazos[3];
-	      		$nr = $pedazos[0].'/'.$pedazos[1].'/'.$pedazos[2].'/'.$nn;
+	      		$nn = 't_' . $pedazos[4];
+	      		$nr = Yii::app()->baseUrl.$pedazos[1].'/'.$pedazos[2].'/'.$pedazos[3].'/'.$nn;
 	      		print_r($pedazos);
 	      		echo $nr;
 	      		Yii::import('application.extensions.image.Image');
-				$image = new Image($foto->src);
+				$image = new Image(Yii::app()->baseUrl.$foto->src);
 				$image->resize(174, 145);
 				$image->render();
 	      	}
