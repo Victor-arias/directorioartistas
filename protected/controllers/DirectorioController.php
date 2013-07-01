efa0793fea14efd13c760b12c7d68ed46f128408
Victor Arias <hugovic1182@gmail.com>
2013-06-30 20:51:29 -0500

    Ya la redimension, pero va por tandas 6

--- protected/controllers/DirectorioController.php
+++ protected/controllers/DirectorioController.php
@@ -205,7 +205,7 @@ class DirectorioController extends Controller
 	{
 		$c = new CDbCriteria;
 		$c->limit = 40;
-		$c->offset = 240;
+		$c->offset = 280;
 		$perfiles = Perfiles::model()->findAll($c);
 	    foreach($perfiles as $perfil)
 	    {
