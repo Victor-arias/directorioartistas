86d377b84849b9e3d8c240f3d6d0a628cef34d9e
Victor Arias <hugovic1182@gmail.com>
2013-07-03 18:49:12 -0500

    Prueba para redimensionar 16

--- protected/controllers/DirectorioController.php
+++ protected/controllers/DirectorioController.php
@@ -232,8 +232,8 @@ class DirectorioController extends Controller
 	public function actionGenerarThumbs()
 	{
 		$c = new CDbCriteria;
-		$c->limit = 70;
-		$c->offset = 230;
+		$c->limit = 30;
+		$c->offset = 270;
 		$perfiles = Perfiles::model()->findAll($c);
 	    foreach($perfiles as $perfil)
 	    {
