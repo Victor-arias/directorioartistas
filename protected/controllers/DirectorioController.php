60c37fd6a4bcd788a1f6bf02c4b0ea449ab110a4
Victor Arias <hugovic1182@gmail.com>
2013-07-03 18:43:04 -0500

    Prueba para redimensionar 11

--- protected/controllers/DirectorioController.php
+++ protected/controllers/DirectorioController.php
@@ -233,7 +233,7 @@ class DirectorioController extends Controller
 	{
 		$c = new CDbCriteria;
 		$c->limit = 50;
-		$c->offset = 50;
+		$c->offset = 100;
 		$perfiles = Perfiles::model()->findAll($c);
 	    foreach($perfiles as $perfil)
 	    {
